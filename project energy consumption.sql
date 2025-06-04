696fc32972c3ad8b955ed1652c9c16c602e6e54d
lipsasneha27 <snehalipsa76@gmail.com>
2025-06-04 17:59:46 +0530

    Add files via upload

---
+++ project energy consumption.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE ENERGYDB2;
+USE ENERGYDB2;
+
+-- 1. country table
+CREATE TABLE country (
+    CID VARCHAR(10) PRIMARY KEY,
+    Country VARCHAR(100) UNIQUE
+);
+
+SELECT * FROM COUNTRY;
+
+-- 2. emission_3 table
+CREATE TABLE emission_3 (
+    country VARCHAR(100),
+    energy_type VARCHAR(50),
+    year INT,
+    emission INT,
+    per_capita_emission DOUBLE,
+    FOREIGN KEY (country) REFERENCES country(Country)
+);
+
+SELECT * FROM EMISSION_3;
+
+
+-- 3. population table
+CREATE TABLE population (
+    countries VARCHAR(100),
+    year INT,
+    Value DOUBLE,
+    FOREIGN KEY (countries) REFERENCES country(Country)
+);
+
+SELECT * FROM POPULATION;
+
+-- 4. production table
+CREATE TABLE production (
+    country VARCHAR(100),
+    energy VARCHAR(50),
+    year INT,
+    production INT,
+    FOREIGN KEY (country) REFERENCES country(Country)
+);
+
+
+SELECT * FROM PRODUCTION;
+
+-- 5. gdp_3 table
+CREATE TABLE gdp_3 (
+    Country VARCHAR(100),
+    year INT,
+    Value DOUBLE,
+    FOREIGN KEY (Country) REFERENCES country(Country)
+);
+
+SELECT * FROM GDP_3;
+
+-- 6. consumption table
+CREATE TABLE consumption (
+    country VARCHAR(100),
+    energy VARCHAR(50),
+    year INT,
+    consumption INT,
+    FOREIGN KEY (country) REFERENCES country(Country)
+);
+
+SELECT * FROM CONSUMPTION;
